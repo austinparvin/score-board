a479f3638e0ee383169203ec9cb35cabd9a22e67
Austin Parvin <austin.parvin@gmail.com>
2020-03-16 12:59:35 -0400

    added update button function

--- public/main.js
+++ public/main.js
@@ -4,4 +4,22 @@ const main = () => {
   }
 }
 
+const updateTeamOneName = () => {
+  const newTeamName = document.querySelector('.team-1-input').value
+  document.querySelector('.team-1-name').textContent = newTeamName
+}
+
+const updateTeamTwoName = () => {
+  const newTeamName = document.querySelector('.team-2-input').value
+  document.querySelector('.team-2-name').textContent = newTeamName
+}
+
 document.addEventListener('DOMContentLoaded', main)
+
+document
+  .querySelector('.update-team-1-name')
+  .addEventListener('click', updateTeamOneName)
+
+document
+  .querySelector('.update-team-2-name')
+  .addEventListener('click', updateTeamTwoName)
